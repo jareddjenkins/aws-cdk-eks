410d0c3c266d95f1bebca45ad57a94c38ec0d5bb
Jared Jenkins <jared.jenkins@contino.io>
2023-01-12 12:36:45 -0700

    add props for cluster

--- lib/cluster-stack.ts
+++ lib/cluster-stack.ts
@@ -3,8 +3,10 @@ import * as iam from '@aws-cdk/aws-iam';
 import * as eks from '@aws-cdk/aws-eks';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import { PhysicalName } from '@aws-cdk/core';
+import { EksProps } from './cluster-stack';
 
 export class ClusterStack extends cdk.Stack {
+  public readonly cluster: eks.Cluster;
 
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -13,26 +15,21 @@ export class ClusterStack extends cdk.Stack {
 
     const clusterAdmin = new iam.Role(this, 'AdminRole', {
       assumedBy: new iam.AccountRootPrincipal()
-      });
+    });
 
     const cluster = new eks.Cluster(this, 'demo-xyz-cluster', {
-        clusterName: `demoxyz`,
-        mastersRole: clusterAdmin,
-        version: eks.KubernetesVersion.V1_21,
-        defaultCapacity: 2
+      clusterName: `demoxyz`,
+      mastersRole: clusterAdmin,
+      version: eks.KubernetesVersion.V1_21,
+      defaultCapacity: 2
     });
 
     cluster.addAutoScalingGroupCapacity('spot-group', {
       instanceType: new ec2.InstanceType('m5.large'),
-      spotPrice: cdk.Stack.of(this).region==primaryRegion ? '0.248' : '0.192'
+      spotPrice: cdk.Stack.of(this).region == primaryRegion ? '0.248' : '0.192'
     });
 
-
-
-
-
-
-
+    this.cluster = cluster;
   }
 }
 
@@ -44,4 +41,8 @@ function createDeployRole(scope: cdk.Construct, id: string, cluster: eks.Cluster
   cluster.awsAuth.addMastersRole(role);
 
   return role;
+}
+
+export interface EksProps extends cdk.StackProps {
+  cluster: eks.Cluster
 }
